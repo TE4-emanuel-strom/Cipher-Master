60453275da86ce8cfe9f9f8841ade601ab13fe42
Emanuel Ström <emanuel.strom@elev.ga.ntig.se>
2019-09-30 22:20:11 +0200

    now have working test class

--- kotlin_cipher/src/main/hello.kt
+++ kotlin_cipher/src/main/hello.kt
@@ -6,10 +6,10 @@ import tests.TestClass
 fun main(){
     val tests = arrayOf<TestClass>(
         TestClass("Testing hello()", "Hello, World!", hello()),
-        TestClass("Testing Failure", "you failed!!!", hello())
+        TestClass("Testing stoopid test", "you failed!!!", "you failed!!!"),
+        TestClass("Other stoopid test", false, 1 == 2)
     )
     TestClass.runTests(tests)
-
 }
 
 fun hello(): String {
--- kotlin_cipher/src/tests/testClass.kt
+++ kotlin_cipher/src/tests/testClass.kt
@@ -5,27 +5,25 @@ open class TestClass(
     private val expectedValue: Any,
     private val actualValue: Any) {
 
-    //companion object TestRunner {
-     fun runTests(listOfTests: Array<TestClass>): Unit {
-        val map = mutableMapOf<String, Boolean>()
-        for (Test in listOfTests) {
-            map[this.note] = this.expectedValue == this.actualValue
+    companion object TestRunner {
+
+        fun runTests(listOfTests: Array<TestClass>): Unit {
+            val map = mutableMapOf<String, Boolean>()
+            for (Test in listOfTests) map[Test.note] = Test.expectedValue == Test.actualValue
+            val numberOfTests = map.size
+
+            println(if (map.containsValue(false)) {
+                val failedTests = map.filter { !it.value }
+                val numberOfFailedTests = numberOfTests - failedTests.size
+                var response = ""
+                for (test in failedTests) {
+                    response += test.key
+                }
+                response += " failed. \n$numberOfFailedTests of $numberOfTests tests succeeded"
+                response
+            } else {
+                "All $numberOfTests tests passed!"
+            })
         }
-
-        val numberOfTests = map.size
-
-        println(if (map.containsValue(false)) {
-            val failedTests = map.filter { it.value }
-            var response: String = ""
-            for (test in failedTests) {
-                response += test.key
-            }
-            response += "failed. \n$failedTests.size of $numberOfTests tests succeeded"
-            response
-        } else {
-            "All $numberOfTests tests passed!"
-        })
-    
     }
-
 }
